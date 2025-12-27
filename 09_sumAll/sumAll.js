8556532056a1ee8202cd36926b52f1ab60ad6683
daaaaawn <215831470+daaaaawn@users.noreply.github.com>
2025-12-27 09:42:05 +0800

    Sum all complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (firstNum < 0 || secondNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let smallestNum = Math.min(firstNum, secondNum);
+    let largestNum = Math.max(firstNum, secondNum);
+
+    for (let i = smallestNum; i <= largestNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
